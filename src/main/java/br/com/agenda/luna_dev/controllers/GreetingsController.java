f4c0cd2dce1a076c384b4b5c07f8567a5b31f549
Bruno Luna <luna.brsilva@gmail.com>
2021-10-22 10:00:57 -0300

    [Inserção do verbo GET - listarTodos]

--- src/main/java/br/com/agenda/luna_dev/controllers/GreetingsController.java
+++ src/main/java/br/com/agenda/luna_dev/controllers/GreetingsController.java
@@ -1,13 +1,20 @@
 package br.com.agenda.luna_dev.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.agenda.luna_dev.model.Usuario;
 import br.com.agenda.luna_dev.repository.UsuarioRepository;
 
 /**
@@ -33,6 +40,28 @@ public class GreetingsController {
     @RequestMapping(value = "/ola/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String ola(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	
+    	userRepository.save(usuario); 
+    	// salva no banco de dados
+    	
+    	
     	return "Olá, " + nome;
     }
+    
+    @GetMapping(value = "/listarTodos")  /*Primeiro metódo da API*/
+    @ResponseBody /*Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	
+     List<Usuario> usuarios = userRepository.findAll();
+     //executa a consulta no banco de dados
+
+     return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+//     return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna a lista em formato JSON*/
+     
+    }
+    
+  
 }
