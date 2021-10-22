4d57a6192d96d9e95f9cb77ff912300e32bb5783
Bruno Luna <luna.brsilva@gmail.com>
2021-10-22 10:38:37 -0300

    [Inserção do verbo POST - salvar]

--- src/main/java/br/com/agenda/luna_dev/controllers/GreetingsController.java
+++ src/main/java/br/com/agenda/luna_dev/controllers/GreetingsController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -63,5 +64,12 @@ public class GreetingsController {
      
     }
     
-  
+    @PostMapping(value = "/salvar") /*Mapeia a URL*/
+    @ResponseBody  /*Descrição da resposta*/
+    public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario){ /* Recebe os dados para salvar*/
+    	
+    	Usuario user = userRepository.save(usuario);
+
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED); /*CREATED = status 201*/
+    }
 }
